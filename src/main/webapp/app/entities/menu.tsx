6fa52f9352ea198625d9aee70e94729181e415a9
Ghaziyassine <ghaziyassine241@gmail.com>
2023-12-23 13:51:34 +0100

    first commit

--- src/main/webapp/app/entities/menu.tsx
+++ src/main/webapp/app/entities/menu.tsx
@@ -7,6 +7,21 @@ const EntitiesMenu = () => {
   return (
     <>
       {/* prettier-ignore */}
+      <MenuItem icon="asterisk" to="/ferme">
+        <Translate contentKey="global.menu.entities.ferme" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/parcelle">
+        <Translate contentKey="global.menu.entities.parcelle" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/plante">
+        <Translate contentKey="global.menu.entities.plante" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/type-plante">
+        <Translate contentKey="global.menu.entities.typePlante" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/plantage">
+        <Translate contentKey="global.menu.entities.plantage" />
+      </MenuItem>
       {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
     </>
   );
